5e73f3f29b976d5f3c23eeaa4304cc996bbaa2d8
navaneesm <navaneethakrishnansmn@gmail.com>
2019-03-09 11:17:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
-const http = require('http');
+// const http = require('http');
 
-const port = process.env.PORT || 5000
-const server = http.createServer((req, res) => {
-	var text = {
+// const port = process.env.PORT || 5000
+// const server = http.createServer((req, res) => {
+// 	var text = {
+// 		text : "ngrok"
+// 	}
+// 	var output = {
+// 		output : text
+// 	}
+// 	var response = {
+// 		status : "Success",
+// 		result : output
+// 	}
+// 	console.log(`incoming`);
+//     res.end(JSON.stringify(response));
+// });
+
+// server.listen(port, (err) => {
+//     if (err) return console.log(`Something bad happened: ${err}`);
+//     console.log(`Node.js server listening on ${port}`);
+
+// });
+
+
+// ----------------- Express -----------------
+
+var express = require('express');
+var app = express();
+// parse application/json
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+   extended: true
+ }))
+//app.use(express.json());
+
+var text = {
 		text : "ngrok"
 	}
 	var output = {
@@ -12,31 +44,9 @@ const server = http.createServer((req, res) => {
 		status : "Success",
 		result : output
 	}
-	console.log(`incoming`);
-    res.end(JSON.stringify(response));
+
+app.post('/', function(req, res){
+  res.json(response);
 });
 
-server.listen(port, (err) => {
-    if (err) return console.log(`Something bad happened: ${err}`);
-    console.log(`Node.js server listening on ${port}`);
-
-});
-
-
-// ----------------- Express -----------------
-
-// var express = require('express');
-// var app = express();
-// // parse application/json
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({
-//    extended: true
-//  }))
-// //app.use(express.json());
-
-
-// app.post('/', function(req, res){
-//   res.json(req.body);
-// });
-
-// app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 5000);
