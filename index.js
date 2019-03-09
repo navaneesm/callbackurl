e8ef63330d8bbc15ae9ae3d9b893302476a632eb
navaneesm <navaneethakrishnansmn@gmail.com>
2019-03-09 12:19:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
-// const http = require('http');
+// // const http = require('http');
 
-// const port = process.env.PORT || 5000
-// const server = http.createServer((req, res) => {
-// 	var text = {
+// // const port = process.env.PORT || 5000
+// // const server = http.createServer((req, res) => {
+// // 	var text = {
+// // 		text : "ngrok"
+// // 	}
+// // 	var output = {
+// // 		output : text
+// // 	}
+// // 	var response = {
+// // 		status : "Success",
+// // 		result : output
+// // 	}
+// // 	console.log(`incoming`);
+// //     res.end(JSON.stringify(response));
+// // });
+
+// // server.listen(port, (err) => {
+// //     if (err) return console.log(`Something bad happened: ${err}`);
+// //     console.log(`Node.js server listening on ${port}`);
+
+// // });
+
+
+// // ----------------- Express -----------------
+
+// var express = require('express');
+// var app = express();
+// var bodyParser = require('body-parser');
+// // parse application/json
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+//    extended: true
+//  }))
+// //app.use(express.json());
+
+// var text = {
 // 		text : "ngrok"
 // 	}
 // 	var output = {
@@ -12,42 +45,44 @@
 // 		status : "Success",
 // 		result : output
 // 	}
-// 	console.log(`incoming`);
-//     res.end(JSON.stringify(response));
+
+// app.post('/', function(req, res){
+//   res.send(req.body);
 // });
 
-// server.listen(port, (err) => {
-//     if (err) return console.log(`Something bad happened: ${err}`);
-//     console.log(`Node.js server listening on ${port}`);
-
-// });
+// app.listen(process.env.PORT || 5000);
 
 
-// ----------------- Express -----------------
+
+
+
+
+
+
+
 
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+
 // parse application/json
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
    extended: true
- }))
-//app.use(express.json());
+ }));
 
-var text = {
-		text : "ngrok"
-	}
-	var output = {
-		output : text
-	}
-	var response = {
-		status : "Success",
-		result : output
-	}
-
+//Handle POST request
 app.post('/', function(req, res){
-  res.send(req.body);
+
+	var handler = req.body.handler;
+	var output = {};
+	if(handler == 1000)
+	{
+		output.text = "Hi navanee ! Slash commands are short cuts to perform tasks. Commands can also provide suggestions, just configure the command suggestion handler! :smile:";
+	}
+
+  	res.send(output);
 });
 
 app.listen(process.env.PORT || 5000);
+
