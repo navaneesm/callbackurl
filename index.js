917a8410442e596384c7a612751f54b6170795a5
navaneesm <navaneethakrishnansmn@gmail.com>
2019-03-09 11:29:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -27,6 +27,7 @@
 
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 // parse application/json
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
