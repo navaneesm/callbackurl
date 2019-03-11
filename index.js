6645659dab7f9a20852db96478a3e62bf7cea69d
navaneesm <navaneethakrishnansmn@gmail.com>
2019-03-11 12:27:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -14,42 +14,75 @@ app.post('/', function(req, res){
 	var handler = req.body.handler;
 	var componentName = req.body.name;
 	var response = {};
-	if(handler == 1000)	//Command execution handler reponse
+	if(handler == 1000)		//Command execution handler reponse
 	{
 		var output = {};
-		if(componentName == "normalCommand")
+		if(componentName == "normalCommand")	//required consents  -- user
 		{
-			output.text = "Hi navanee ! Slash commands are short cuts to perform tasks. Commands can also provide suggestions, just configure the command suggestion handler! :smile:";	
+			output = {"text":"Hi " + req.body.params.user.first_name + " ! Slash commands are short cuts to perform tasks. Commands can also provide suggestions, just configure the command suggestion handler! :smile:"};
 		}
-		else if(componentName == "suggestionCommand")
+		else if(componentName == "suggestionCommand")	//required consents  -- 
 		{
-			output.text = "Hi you have selected " + req.body.params.selections.length + " suggestions";	
+			output = {"text":"Hi you have selected " + req.body.params.selections.length + " suggestions and first title is " + req.body.params.selections[0].title};	
 		}
-		else if(componentName == "invokeFunctionCmd")
+		else if(componentName == "invokeFunctionCmd")	//required consents  -- 
 		{
-// 			var inputs = [];
-// 			inputs.push({"type":"text","name":"username","label":"Name","hint":"Please enter your name","placeholder":"Scott Fisher","mandatory":true,"value":"Scott Fisher"});
-// 			inputs.push({"type":"text","format":"email","name":"email","label":"Email","hint":"Enter your email address","placeholder":"scott.fisher@zylker.com","mandatory":true,"value":"scott.fisher@zylker.com"});
-// 			inputs.push({"type":"select","trigger_on_change":true,"name":"asset-type","label":"Asset Type","hint":"Choose your request asset type.","placeholder":"Mobile","mandatory":true,"options":[{"label":"Laptop","value":"laptop"},{"label":"Mobile","value":"mobile"}]});
-// 			var forms = {"type":"form","title":"Asset Request","hint":"Raise your asset request.","name":"ID","version":1,"button_label":"Raise Request","actions":{"submit":{"type":"invoke.function","name":"urlFunForm"}},"inputs":inputs};
-// 			output.forms = forms;
 			output = {"version":1,"inputs":[{"label":"Name","name":"username","type":"text","value":"Scott Fisher","mandatory":true,"placeholder":"Scott Fisher","hint":"Please enter your name"},{"label":"Email","name":"email","type":"text","value":"scott.fisher@zylker.com","mandatory":true,"format":"email","placeholder":"scott.fisher@zylker.com","hint":"Enter your email address"},{"label":"Asset Type","name":"asset-type","options":[{"label":"Laptop","value":"laptop"},{"label":"Mobile","value":"mobile"}],"type":"select","trigger_on_change":true,"mandatory":true,"placeholder":"Mobile","hint":"Choose your request asset type."}],"name":"ID","type":"form","button_label":"Raise Request","actions":{"submit":{"name":"function","type":"invoke.function"}},"title":"Asset Request","hint":"Raise your asset request."};
 		}
 	}
-	else if(handler == 1001)	//Command suggestion handler reponse
+	else if(handler == 1001)	//Command suggestion handler response.     			//required consents  -- 
 	{
-		var output = [];
-		var entry = {};
-		entry.title = "Tip 1 👋";
-		entry.description = "Command suggestions are helpful when you have to choose from a list of entities!";
-		entry.imageurl = "https://media3.giphy.com/media/Cmr1OMJ2FN0B2/giphy.gif";
-		output.push(entry);
-		var entry1 = {};
-		entry1.title = "Tip 2 😲";
-		entry1.description = "You can show upto a maximum of 50 command suggestions. :surprise:";
-		entry1.imageurl = "https://media2.giphy.com/media/8uzVsRzOScAa4/giphy.gif";
-		output.push(entry1);
+		output = [{"description":"Command suggestions are helpful when you have to choose from a list of entities!","imageurl":"https://media3.giphy.com/media/Cmr1OMJ2FN0B2/giphy.gif","title":"Tip 1 👋"},{"description":"You can show upto a maximum of 50 command suggestions. :surprise:","imageurl":"https://media2.giphy.com/media/8uzVsRzOScAa4/giphy.gif","title":"Tip 2 😲"}];
 	}
+	else if(handlerType.equals(2001))	//Form submit handler response
+	{
+
+		form = req.body.params.form;
+		formValues = form.values;
+		if(formValues.asset-type.get.value == "mobile")
+		{
+			output = {"text":"Hi " + formValues.username + ", thanks for raising your request. Your request will be processed based on the device availability.","card":{"title":"Asset Request"},"slides":[{"type":"label","title":"","data":[{"Asset Type":formValues.asset-type.label},{"Preferred OS ":formValues.mobile-os.label},{"Preferred Device":formValues.mobile-list.label}]}]};
+		}
+		else
+		{
+			output = {"text":"Hi " + formValues.username + ", thanks for raising your request. Your request will be processed based on the device availability.","card":{"title":"Asset Request"},"slides":[{"type":"label","title":"","data":[{"Asset Type":formValues.asset-type.label},{"OS/Device Preferred":formValues.os-type.label}]}]};
+		}
+	}
+	else if(handlerType.equals(2002))	//Form change handler response
+	{
+		var targetName = req.body.params.target.name;
+		var inputValues = req.body.params.form.get.values;
+		var actions = [];
+		if(targetName  == "asset-type")
+		{
+			fieldValue = inputValues.asset-type.value;
+			if(fieldValue == "laptop")
+			{
+				actions.push({"type":"add_after","name":"asset-type","input":{"type":"select","name":"os-type","label":"Laptop Type","hint":"Choose your preferred OS Type","placeholder":"Ubuntu","mandatory":true,"options":[{"label":"Mac OS X","value":"mac"},{"label":"Windows","value":"windows"},{"label":"Ubuntu","value":"ubuntu"}]}});
+				actions.push({"type":"remove","name":"mobile-os"});
+				actions.push({"type":"remove","name":"mobile-list"});
+			}
+			else if(fieldValue == "mobile")
+			{
+				actions.push({"type":"add_after","name":"asset-type","input":{"type":"select","name":"mobile-os","label":"Mobile OS","hint":"Choose your preferred mobile OS","mandatory":true,"placeholder":"Android","options":[{"label":"Android","value":"android"},{"label":"iOS","value":"ios"}],"trigger_on_change":true}});
+				actions.push({"type":"remove","name":"os-type"});
+			}
+		}
+		else if(targetName == "mobile-os")
+		{
+			fieldValue = inputValues.asset-type.value;
+			if(typeof fieldValue !== "undefined")
+			{
+				actions.push({"type":"add_after","name":"mobile-os","input":{"type":"dynamic_select","name":"mobile-list","label":"Mobile Device","mandatory":true,"placeholder":"Choose your preferred mobile device"}});
+			}
+			else
+			{
+				actions.push({"type":"remove","name":"mobile-list"});
+			}
+		}
+		output = {"type":"form_modification","actions":actions};
+	}
+
 	response.output = output;
   	res.send(response);
 });
